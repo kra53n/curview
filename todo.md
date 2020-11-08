1cc160761d45b5fe4082876828fc5c7d1e6ad664
Krai53n <greasha46@gmail.com>
2020-11-08 13:19:25 +0300

    (￢_￢;)

---
+++ todo.md
@@ -0,0 +1,4 @@
++ think more about working withd data base. For example we can have one data base "curview.md"
+  and different tables with years. Or wwe can have data base with numerues of year "2020"
+  and tables with different currency dollar, bitcoin and e.t.c.
+- think more about message in wwdb.py in wwdb(). Maybe will better put it in funcion?
---
+++ wwdb.py
@@ -0,0 +1,31 @@
+# WWDB - WORK WITH DATA BASE
+
+import sqlite3 as sql
+from modules.date import exactly_time as ex_t
+
+
+### CONSTANTS
+NAME_DB = str(ex_t()[0])
+
+
+### FUNTIOCNS
+def write_to_db():
+    pass
+
+def wwdb(currency, choice):
+    '''Function write or recieve data from data base
+    ================================================
+    Argument currency get type of currency
+    ================================================
+    Argument choice:
+        choice = 0 or choice = 1
+        choice = 0 --> write
+        choice = 1 --> recieve
+    '''
+    con = sql.connect(NAME_DB)
+    # TODO: finish message
+    message = 'CREATE TABLE IF NOT EXISTS '
+    message += '`{}` (`name` STRING, `surname` STRING)'.format(currency)
+    with con:
+        cur = con.cursor()
+        cur.execute('CREATE TABLE IF NOT EXISTS `test` (`name` STRING, `surname` STRING)')
