b760d706990432c886abb7493a3d87edeb48bbd2
Krai53n <greasha46@gmail.com>
2020-11-13 13:04:00 +0300

    ┐(￣∀￣)┌

--- data.py
+++ data.py
@@ -11,147 +11,49 @@ def __try_again():
     input('I`m sorry but you type something strange! ')
     input('Let`s try again! ')
 
-def __dollar(ask):
-    '''Function which will work if we choose dollar
-    function return course of dollar in rubles and
-    our money in rubles
+def __distrebute_currency(ask, cur):
+    ''' Function distrebute currunecy on what we need
+    for example we need in ifnormation of dlr, rub
+    and e.t.c.
+    =====================================================
+    Args:
+        ask:
+            ask = 0 --> we don`t need in numerous of cur
+            ask = 1 --> we need in numeriys if cur
+        cur --> type of currency
     '''
+    num = 0
+    row = ('bitcoin', 'ethereum')
     if ask == 1:
-        num = input('How many dollars you have? --> ')
-    try:
-        if ask == 1:
-            num = float(num)
-        else:
-            num = 0
-        course_dollar_rub = dollar
-        dollar_rub = course_dollar_rub * num
-        return course_dollar_rub, dollar_rub
-    except ValueError:
-        __try_again()
-
-def __bitcoin(ask):
-    '''Function which will work if we choose bictcoin
-    function return course of btc in dollars, course btc in rub,
-    our money in dollars and our money in rubles
-    '''
-    if ask == 1:
-        num = input('How many bitcoins you have --> ')
-    try:
-        if ask == 1:
-            num = float(num)
-        else:
-            num = 0
-        course_btc_dollar = bitcoin
-        course_btc_rub = course_btc_dollar * dollar
-        btc_dollars = course_btc_dollar * num
-        btc_rubles = course_btc_rub * num
-        return course_btc_dollar, course_btc_rub, btc_dollars, btc_rubles
-    except ValueError:
-        __try_again()
-
-def __ethereum(ask):
-    '''Function which will work if we choose bictcoin
-    function return course of btc in dollars, course btc in rub,
-    our money in dollars and our money in rubles
-    '''
-    if ask == 1:
-        num = input('How many ethereum you have --> ')
-    try:
-        if ask == 1:
-            num = float(num)
-        else:
-            num = 0
-        course_eth_dollar = ethereum
-        course_eth_rub = course_eth_dollar * dollar
-        eth_dollars = course_eth_dollar * num
-        eth_rubles = course_eth_rub * num
-        return course_eth_dollar, course_eth_rub, eth_dollars, eth_rubles
-    except ValueError:
-        __try_again()
-
-
-def __gold(ask):
-    '''Function which will work if you choose gold in option
-    function return course of gold in rubles and our money in rub
-    '''
-    if ask == 1:
-        num = input('How many gold you have --> ')
-    try:
-        if ask == 1:
-            num = float(num)
-        else:
-            num = 0
-        course_gold_rubles = gold
-        gold_rubles = course_gold_rubles * num
-        return course_gold_rubles, gold_rubles
-    except ValueError:
-        __try_again()
-
-def __silver(ask):
-    '''Function which will work if you choose silver
-    in option
-    function return course of silver in rub and our money in rub
-    '''
-    if ask == 1:
-        num = input('How many silver you have --> ')
-    try:
-        if ask == 1:
-            num = float(num)
-        else:
-            num = 0
-        course_silver_rubles = silver
-        silver_rubles = course_silver_rubles * num
-        return course_silver_rubles, silver_rubles
-    except:
-        __try_again()
-
-def __palladium(ask):
-    '''Function which will work if we choose
-    option 6(palladium)
-    function return course of palladium in rub and our money in run
-    '''
-    if ask == 1:
-        num = input('How many palladium you have --> ')
-    try:
-        if ask == 1:
-            num = float(num)
-        else:
-            num = 0
-        course_palladium_rubles = palladium
-        palladium_rubles = course_palladium_rubles * num
-        return course_palladium_rubles, palladium_rubles
-    except:
-        __try_again()
+        num = float(input(f'How many {cur} you have --> '))
+    course_rub = eval(cur)
+    rub = course_rub * num
+    if cur in row:
+        course_dlr = eval(cur)
+        course_rub = course_dlr * dollar
+        dlr = course_dlr * num
+        rub = course_rub * num
+        return course_dlr, course_rub, dlr, rub
+    else:
+        return course_rub, rub
 
 def __exec_option(option, ask):
-    # TODO write comment to function
-    '''Function that
+    '''Function translate type of currency to __destrebute_currence
     '''
-    if option == 1:
-        name = 'dollar'
-        course_rub, rub = __dollar(ask)
-    if option == 2:
-        name = 'bitcoin'
-        course_dlr, course_rub, dlr, rub = __bitcoin(ask)
-    if option == 3:
-        name = 'ethereum'
-        course_dlr, course_rub, dlr, rub = __ethereum(ask)
-    if option == 4:
-        name = 'gold'
-        course_rub, rub = __gold(ask)
-    if option == 5:
-        name = 'silver'
-        course_rub, rub = __silver(ask)
-    if option == 6:
-        name = 'palladium'
-        course_rub, rub = __palladium(ask)
-    try:
-        if option == 2 or option == 3:
-            return name, course_dlr, course_rub, dlr, rub
-        else:
-            return name, course_rub, rub
-    except:
-        pass
+    curs = ('dollar', 'bitcoin', 'ethereum', 'gold', 'silver', 'palladium')
+    row = ('bitcoin', 'ethereum')
+    for i in range(1, 7):
+        if i == option:
+            name = curs[i-1]
+            if name in row:
+                course_dlr, course_rub, dlr, rub = __distrebute_currency(ask,
+                                                                         name)
+            else:
+                course_rub, rub = __distrebute_currency(ask, name)
+    if name in row:
+        return name, course_dlr, course_rub, dlr, rub
+    else:
+        return name, course_rub, rub
 
 def options(option = None, ask = 0):
     '''Function ask which value you whant pass
