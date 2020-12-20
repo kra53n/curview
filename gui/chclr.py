bd8e8d94fdf1c665fd8b0be620e3c5ed99cbd7ea
Krai53n <greasha46@gmail.com>
2020-12-20 19:40:36 +0300

    (╥_╥)

---
+++ gui/chclr.py
@@ -0,0 +1,36 @@
+'''
+Help to choose color for gui
+'''
+
+from tkinter import Tk, Label
+from json import load
+
+
+class ChclrGui:
+    def __init__(self, master):
+        self.master = master
+        self.master.resizable(0, 0)
+        self.master.configure(background = '#111')
+        self.create_widgets()
+
+    def create_widgets(self):
+        m = load_colors()
+        for i in range(len(m)):
+            Label(self.master,
+                   bd = 0,
+                   text = m[i][0],
+                   background = m[i][1],
+                   foreground = '#111',
+                   ).pack(fill = 'x', ipady = 10, pady = 2)
+
+### FUNTCIONS
+def load_colors():
+    with open('colors.json', 'r') as f:
+        data = load(f)
+    return tuple(data.items())
+
+
+if __name__ == '__main__':
+    root = Tk()
+    gui = ChclrGui(root)
+    root.mainloop()
