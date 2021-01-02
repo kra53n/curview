aa633fdc6aabd246837c2eb0f960740d2abb68f2
Krai53n <greasha46@gmail.com>
2021-01-02 21:22:23 +0300

    (╥_╥)

--- api/constants.py
+++ api/constants.py
@@ -1,19 +1,3 @@
 # currencies that have difference btw other(bit and dlr)
 CUR_ROW = ('ethereum', # other currency have less details
         'bitcoin')
-
-CURS = ('dollar',
-        'bitcoin',
-        'ethereum',
-        'gold',
-        'silver',
-        'palladium')
-
-CUR_CHOOSE_TEXT = '''
-    1 - dollar
-    2 - bitcoin
-    3 - ethereum
-    4 - gold
-    5 - silver
-    6 - palladium
-'''
