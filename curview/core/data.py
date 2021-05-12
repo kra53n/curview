a6e24bf38ca3107f58da2d7401cfabced9a7712a
Krai53n <greasha46@gmail.com>
2021-05-12 17:30:53 +0300

    formal changes

--- curview/core/data.py
+++ curview/core/data.py
@@ -13,16 +13,20 @@ for i in range(len(CUR_PARS)):
 def cur_to_dict(cur, amount = 0):
     '''Function create dict with full information of currency
     '''
-    data = {'date': date,
-            'name': cur,
-            'amount': amount,
-            }
+    data = {
+        'date': date,
+        'name': cur,
+        'amount': amount,
+    }
+
     if cur in CUR_ROW:
         data['course_dlr'] = eval(cur)
         data['dlr'] = eval(cur) * amount
         data['course_rub'] = round((eval(cur) * dollar), 4)
         data['rub'] = round((eval(cur) * dollar * amount), 4)
+
     if cur not in CUR_ROW:
         data['course_rub'] = round(eval(cur), 4)
         data['rub'] = round(eval(cur) * amount, 4)
+
     return data
