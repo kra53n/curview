f69a1f3c10d7d12886da6560d50a2c5dbc2929b7
Krai53n <greasha46@gmail.com>
2021-06-14 19:19:29 +0300

    add menu elements

--- curview/gui_tk.py
+++ curview/gui_tk.py
@@ -5,18 +5,41 @@ from gui_tk_utils.add_window import AddWindow
 from core import cur_names_get
 
 
+#######################################################
+# menu widget developing
+
+from gui_tk_utils.menu_widget import Menu
+
+def print_hello():
+    print("Hello (o´'∀'｀o)!")
+
+
+from tkinter import Tk
 root = Tk()
+options_and_actions = (
+    ("main", print_hello),
+    ("graphics", print_hello),
+)
+Menu(root, options_and_actions).pack()
+root.mainloop()
 
-path="configs"
-filename="parse.yaml"
 
-AddWindow(
-    root,
-    cur_names=cur_names_get(
+#######################################################
+
+
+def unnecessary():
+    root = Tk()
+
+    path="configs"
+    filename="parse.yaml"
+
+    AddWindow(
+        root,
+        cur_names=cur_names_get(
+            path=path,
+            filename=filename,
+        ),
         path=path,
         filename=filename,
-    ),
-    path=path,
-    filename=filename,
-).pack()
-root.mainloop()
+    ).pack()
+    root.mainloop()
